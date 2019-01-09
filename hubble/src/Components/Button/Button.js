d8e0d5fe8e2c3d118c0c18ae4a952350c667286f
Amber <amberrose9167@gmail.com>
2019-01-08 19:05:38 -0800

    added buttons and started on landing page components

--- hubble/src/Components/Button/Button.js
+++ hubble/src/Components/Button/Button.js
@@ -1,6 +1,34 @@
 import React from "react";
 import "./Button.css";
 
-const Button = props=>(
-    
-)
+const SignInButton = props => (
+    <span className="signInBtn"{...props}>
+        Sign In
+    </span>
+);
+const SignUpButton = props => (
+    <span className="signUpBtn"{...props}>
+        Sign Up
+    </span>
+);
+const SignOutButton = props => (
+    <span className="signOutBtn"{...props}>
+        Sign Out
+    </span>
+);
+const YourHobbiesButton = props => (
+    <span className="hobbyBtn"{...props}>
+        See Your Hobbies
+    </span>
+);
+const SubmitButton = props => (
+    <span className="submitBtn"{...props}>
+        Submit
+    </span>
+);
+
+export default SignInButton;
+export default SignUpButton;
+export default SignOutButton;
+export default YourHobbiesButton;
+export default SubmitButton;
---
+++ hubble/src/utils/API.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default {
+  // Gets all Questions
+  getQuestions: function() {
+    return axios.get("/api/users");
+  },
+  getUser: function(id) {
+    return axios.get("/api/users" + id);
+  },
+  getHobbies: function() {
+    return axios.get("/api/users");
+  },
+  deleteUser: function(id){
+      return axios.delete("/api/users" + id)
+  },
+  saveUser: function(userData){
+      return axios.post("/api/users", userData)
+  }
+//   delete specific hobby from list
+};
