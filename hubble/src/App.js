8aee0f3681a2b55bf28848a72f0b7b31cffb8518
Amber <amberrose9167@gmail.com>
2019-01-10 21:49:31 -0800

    finished adding the questions and changed some of the css

--- hubble/src/App.js
+++ hubble/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import User from './pages/landing/landing'
+import Home from './pages/landing/landing';
+import Profile from './pages/profile/profile';
+import Survey from './pages/survey/survey';
+import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-     <User />
-    );
-  }
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/survey" component={Survey} />
+          <Route path="/profile/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
+
+{/* <Route path="/details/" component={Detail} />
+<Route component={NoMatch} /> */}
